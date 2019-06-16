fba9c2d38e57d4fab959fc24afa2c678f0cd27d5
LabiszKa <https://github.com/LabiszKa/katarzyna-labisz-kodilla-java>
2019-06-16 23:03:10 +0200

    Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,7 +1,19 @@
 package com.kodilla;
 
 public class Calculator {
+
+    int a;
+    int b;
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+    public int substractAFromB(double a, double b) {
+        return (int) (a - b);
+    }
     public static void main (String [] args) {
+        Calculator aCalculator = new Calculator();
+        int result = aCalculator.addAToB(4, 9);
+        System.out.println(result);
 
     }
 }

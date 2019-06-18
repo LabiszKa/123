d90b854383964a311056c78cd2ab53872838fbb4
LabiszKa <https://github.com/LabiszKa/katarzyna-labisz-kodilla-java>
2019-06-18 22:33:35 +0200

    SimpleCalculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.Calculator;
+
+public class SimpleCalculator {
+
+    int a;
+    int b;
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+    public int substractAFromB(double a, double b) {
+        return (int) (a - b);
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,23 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+import com.kodilla.testing.Calculator.SimpleCalculator;
 
+public class TestingMain {
+    public static void main(String[] args) {
+        SimpleCalculator aCalculator = new SimpleCalculator();
+        int result = aCalculator.addAToB(4, 9);
+        if (result == 13) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Not ok");
+
+            int subResult = aCalculator.substractAFromB(13, 4);
+            if (subResult ==9) {
+                System.out.println("test 2 ok");
+            } else {
+                System.out.println("test 2 not ok");
+            }
+        }
     }
+
 }

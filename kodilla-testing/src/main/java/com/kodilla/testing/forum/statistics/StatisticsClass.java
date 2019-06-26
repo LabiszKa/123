648c2a4de1fac0cff5db3069fc140ff72183ec77
LabiszKa <https://github.com/LabiszKa/katarzyna-labisz-kodilla-java>
2019-06-26 21:01:52 +0200

    Forum Statistics

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsClass.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsClass.java
@@ -2,43 +2,53 @@ package com.kodilla.testing.forum.statistics;
 
 public class StatisticsClass {
 
-    int usersCount;
-    int postsCount;
-    int commentsCount;
-    double averagePostsQuantity;
-    double averageCommentsQuantity;
-    double averageComPerPostQuantity;
-
-    public StatisticsClass(int usersCount, int postsCount, int commentsCount) {
-//        this.usersCount = usersQuantity;
-//        this.postsCount = postsQuantity;
-//        this.commentsCount = commentsQuantity;
-        }
-
-    public int getUsersCount() {
-        return usersCount;
-    }
-
-    public int getPostsCount() {
-        return postsCount;
-    }
-
-    public int getCommentsCount() {
-        return commentsCount;
-    }
+    double usersQuantity;
+    double postsQuantity;
+    double commentsQuantity;
+    double averagePostsPerUserQuantity;
+    double averageCommentsPerUserQuantity;
+    double averageCommentsPerPostQuantity;
 
     public void calculateAdvStatistics(Statistics statistics) {
+            usersQuantity = statistics.usersNames().size();
+            postsQuantity = statistics.postsCount();
+            commentsQuantity = statistics.commentsCount();
 
-
-        if (getUsersCount() > 0) {
-            averagePostsQuantity = getPostsCount() / getUsersCount();
+        if (postsQuantity <= 0) {
+            averageCommentsPerPostQuantity = 0;
+        } else {
+            averageCommentsPerPostQuantity = commentsQuantity / postsQuantity;
         }
-
-        if (getUsersCount() > 0) {
-            averageCommentsQuantity = getCommentsCount() / getUsersCount();
-        }
-
-        if(getPostsCount() >0)
-            averageComPerPostQuantity = getCommentsCount() / getPostsCount();
+        if (usersQuantity <= 0) {
+            averagePostsPerUserQuantity = 0;
+            averageCommentsPerUserQuantity = 0;
+        } else {
+            averagePostsPerUserQuantity = postsQuantity / usersQuantity;
+            averageCommentsPerUserQuantity = commentsQuantity / usersQuantity;
         }
     }
+
+    public double getUsersQuantity() {
+        return usersQuantity;
+    }
+
+    public double getPostsQuantity() {
+        return postsQuantity;
+    }
+
+    public double getCommentsQuantity() {
+        return commentsQuantity;
+    }
+
+    public double getAveragePostsPerUserQuantity() {
+        return averagePostsPerUserQuantity;
+    }
+
+    public double getAverageCommentsPerUserQuantity() {
+        return averageCommentsPerUserQuantity;
+    }
+
+    public double getAverageCommentsPerPostQuantity() {
+        return averageCommentsPerPostQuantity;
+    }
+}
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/MockStatistics.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/MockStatistics.java
@@ -1,14 +1,182 @@
 package com.kodilla.testing.forum.statistics;
-
+import org.junit.Assert;
+import org.junit.Test;
 import java.util.ArrayList;
-
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MockStatistics {
-// @Test
-    // given
-    Statistics statsMock = mock(Statistics.class);
-    ArrayList<String> usersMock = new ArrayList<String>();
 
+    @Test
+    public void testCalculateAdvStatisticsIfPostsCountIsZero() {
+        // given
+        int postsMock = 0;
+        int commentsMock = 10;
+        ArrayList<String> usersMock = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            usersMock.add("UserName");
+        }
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(10, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(0, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(0.1, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
 
+    @Test
+    public void testCalculateAdvStatisticsIfPostsCountIsThousand() {
+        // given
+        int postsMock = 1000;
+        int commentsMock = 10;
+        ArrayList<String> usersMock = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            usersMock.add("UserName");
+        }
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(1000, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(10, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(0.01, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(0.1, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(10, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsIfCommentsCountIsZero() {
+        // given
+        int postsMock = 10;
+        int commentsMock = 0;
+        ArrayList<String> usersMock = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            usersMock.add("UserName");
+        }
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(10, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(0, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(0.1, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsIfThereIsMoreCommentsThanPosts() {
+        // given
+        int postsMock = 10;
+        int commentsMock = 100;
+        ArrayList<String> usersMock =new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            usersMock.add("UserName");
+        }
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(10, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(10, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(1, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(0.1, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsIfThereIsLessCommentsThanPosts() {
+        // given
+        int postsMock = 100;
+        int commentsMock = 10;
+        ArrayList<String> usersMock = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            usersMock.add("UserName");
+        }
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(100, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(10, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(0.1, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(0.1, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(1, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
+
+    @Test
+    public void testCalculateAdvStatisticsIfThereAreNoUsers() {
+        // given
+        int postsMock = 0;
+        int commentsMock = 0;
+        ArrayList<String> usersMock = new ArrayList<String>();
+
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(0, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(0, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(0, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
+    @Test
+    public void testCalculateAdvStatisticsIfThereAreHundredUsers() {
+        // given
+        int postsMock = 100;
+        int commentsMock = 100;
+        ArrayList<String> usersMock = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            usersMock.add("UserName");
+        }
+
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.postsCount()).thenReturn(postsMock);
+        when(statisticsMock.usersNames()).thenReturn(usersMock);
+        when(statisticsMock.commentsCount()).thenReturn(commentsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        //When
+        statisticsClass.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(100, statisticsClass.getPostsQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getUsersQuantity(), 0.01);
+        Assert.assertEquals(100, statisticsClass.getCommentsQuantity(),0.01);
+        Assert.assertEquals(1, statisticsClass.getAverageCommentsPerPostQuantity(), 0.01);
+        Assert.assertEquals(1, statisticsClass.getAverageCommentsPerUserQuantity(), 0.01);
+        Assert.assertEquals(1, statisticsClass.getAveragePostsPerUserQuantity(), 0.01);
+    }
 }
